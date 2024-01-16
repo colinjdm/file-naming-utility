fa1b96d50e9010e83760636a732728e36b352c4f
colinjdm <colinjdmitchell@gmail.com>
2024-01-15 19:44:45 -0600

    Edits to run on local pc

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import csv
+import sqlite3
 
-from cs50 import SQL
 from flask import Flask, redirect, render_template, request
 
 
 app = Flask(__name__)
 
 # Configure SQLite database
-db = SQL("sqlite:///filenaming.db")
+db = sqlite3.connect("filenaming.db")
 
 
 @app.route("/", methods=["GET", "POST"])
